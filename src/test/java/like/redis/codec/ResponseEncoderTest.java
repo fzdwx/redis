f08dd8fcf590a86e905b0ff38d018c7dbf9adcc4
like <980650920@qq.com>
2022-01-01 11:52:45 +0800

    :white_check_mark: 设置Java版本

---
+++ src/test/java/like/redis/codec/ResponseEncoderTest.java
@@ -0,0 +1,17 @@
+package like.redis.codec;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author <a href="mailto:likelovec@gmail.com">like</a>
+ * @date 2022/1/1 11:50
+ */
+class ResponseEncoderTest {
+
+    @Test
+    void encode() {
+        // TODO: 2022/1/1 ..
+    }
+}
